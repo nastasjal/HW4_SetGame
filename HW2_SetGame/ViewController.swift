8249fea3280c0b7ff55670f58e8dd432842a30cd
nastasjal <nastasjal@list.ru>
2018-11-27 22:18:16 +0300

    Initial Commit

---
+++ HW2_SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2_SetGame
+//
+//  Created by Анастасия Латыш on 27/11/2018.
+//  Copyright © 2018 Анастасия Латыш. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
